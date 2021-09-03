bdcdfb4fc0baa351720a3137c9942cfa5271292a
TalviaTasnim <60611235+TalviaTasnim@users.noreply.github.com>
2021-09-03 13:00:54 +0600

    Autocorrelation

--- Talvia-Tasnim-Prante-1711206642/Code/File Reader/src/FileReader.java
+++ Talvia-Tasnim-Prante-1711206642/Code/File Reader/src/FileReader.java
@@ -11,15 +11,15 @@ public class FileReader {
 
 		
 
-		File file = new File("/Users/xeonc/Desktop/download.txt");
+		File file = new File("/Users/xeonc/Desktop/page 1.txt"); //reading the file
 		Scanner scan = new Scanner(file);
 
-		String fileContent = "";
-		while (scan.hasNextLine()) {
+		String fileContent = "";  // storing the file into a string 
+		while (scan.hasNextLine()) {         //reads the whole file content line by line
 			fileContent = fileContent.concat(scan.nextLine() + "\n");
 		}
 
-		FileWriter writer = new FileWriter("/Users/xeonc/Desktop/newfile.csv");
+		FileWriter writer = new FileWriter("/Users/xeonc/Desktop/newfile.csv"); // converting into csv file
 		writer.write(fileContent);
 		writer.close();
 
